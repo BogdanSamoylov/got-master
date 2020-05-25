9cad6c667eff622bc5c40d07571fc9ff8096a814
Bohdan <samoylov.bogdan@gmail.com>
2020-05-25 14:09:50 +0300

    'second'

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import            './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class = "loadingio-spinner-spin-4jaiw40lt">
+            <div class = "ldio-s34gd5bo6ij"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+        </div>
+    )
+}
+
+export default Spinner;
